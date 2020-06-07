f9593629a84fae77eb5c08f2b75af759f7e63919
bradhoward1 <bradley.howard@duke.edu>
2020-06-07 14:32:30 -0400

    Implemented function to convert a list of strings into a list of floats

--- ecg_analysis.py
+++ ecg_analysis.py
@@ -11,7 +11,26 @@ def import_name():
 
 
 def import_data(filename):
-    pass
+    contents = list()
+    with open(filename, 'r') as input_file:
+        file = input_file.readlines()
+        for line in file:
+            line = line.strip("\n")
+            output = string_split(line)
+            output = str_to_float(output)
+            contents.append(output)
+        return contents
+
+
+def str_to_float(input_results):
+    data = list()
+    for i in input_results:
+        try:
+            i = float(i)
+        except ValueError:
+            print("Not a convertable string")
+        data.append(i)
+    return data
 
 
 def string_split(string):
--- test_ecg_analysis.py
+++ test_ecg_analysis.py
@@ -13,3 +13,14 @@ def test_string_split(result, expected):
     from ecg_analysis import string_split
     answer = string_split(result)
     assert answer == expected
+
+
+@pytest.mark.parametrize("result, expected",
+                         [(["8", "0.9"], [8, 0.9]),
+                          (["word", "0.9"], ["word", 0.9]),
+                          (["word"], ["word"]),
+                          (["8"], [8])])
+def test_str_to_float(result, expected):
+    from ecg_analysis import str_to_float
+    answer = str_to_float(result)
+    assert answer == expected
