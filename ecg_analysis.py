d232a991e8af85a67e7af9ed8647e047f3b9fb6c
bradhoward1 <bradley.howard@duke.edu>
2020-06-07 14:03:45 -0400

    Implemented function that reads in a string and outputs a comma-separated list

--- ecg_analysis.py
+++ ecg_analysis.py
@@ -5,10 +5,19 @@ import json
 import sys
 
 
-def import_data():
+def import_name():
     filename = sys.argv[1]
     return filename
 
 
+def import_data(filename):
+    pass
+
+
+def string_split(string):
+    output = string.split(",")
+    return output
+
+
 if __name__ == '__main__':
-    filename = import_data()
+    filename = import_name()
--- test_ecg_analysis.py
+++ test_ecg_analysis.py
@@ -1,1 +1,15 @@
 # test_ecg_analysis.py
+
+
+import pytest
+
+
+@pytest.mark.parametrize("result, expected",
+                         [("0.003,0.30983", ["0.003", "0.30983"]),
+                          ("0.45", ["0.45"]),
+                          ("word,0.9", ["word", "0.9"]),
+                          ("NaN,0.10", ["NaN", "0.10"])])
+def test_string_split(result, expected):
+    from ecg_analysis import string_split
+    answer = string_split(result)
+    assert answer == expected
