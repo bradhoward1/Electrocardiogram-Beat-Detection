feadff8da641818cf2415db56e4812081a7f4af0
bradhoward1 <bradley.howard@duke.edu>
2020-06-07 13:43:42 -0400

    Function implemented to read in the file name from the command line

--- ecg_analysis.py
+++ ecg_analysis.py
@@ -1,1 +1,14 @@
 # ecg_analysis.py
+
+
+import json
+import sys
+
+
+def import_data():
+    filename = sys.argv[1]
+    return filename
+
+
+if __name__ == '__main__':
+    filename = import_data()
